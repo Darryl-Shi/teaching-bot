2c86baf8b2d586f296f6457251c3abfe879a2a42
Darryl-Shi <221474B@student.hci.edu.sg>
2023-03-05 13:02:43 +0000

    v2 done!

--- main.py
+++ main.py
@@ -12,14 +12,28 @@ class TutorAI:
         self.subtopics = []
         self.messages = []
         self.token = int(os.getenv("TOKEN"))
-        self.prompt_stages = ["A student wants to learn about a topic, generate 4 modules that a student can use to learn and number it. A module consists of a title and a description, separated by a colon. The topic is ", "Create an outline with 4 sections for teaching a student about the topic and module and number it. If you receive the input 'back', go back to the previous stage.","Teach a student about the below topic and subtopic and by writing multiple paragraphs and number it. If you receive the input 'back', go back to the previous stage"]
+        self.prompt_stages = ["A student wants to learn about a topic, generate 4 modules that a student can use to learn and number it. A module consists of a title and a description, separated by a colon. The topic that the student wants to learn about is ", "Create an outline with 4 sections for teaching a student about the topic and module and number it. If you receive the input 'back', go back to the previous stage.","Teach a student about the below topic and subtopic and by writing multiple paragraphs and number it. If you receive the input 'back', go back to the previous stage"]
+        self.updated_prompt_stages =[]
         self.topic = ""
         openai.api_key = self.api_key
 
     def add_topic(self, topic):
-        self.prompt_stages[0] = self.prompt_stages[0]+topic
+        for i in range(len(self.prompt_stages)):
+            if i == 0:
+                self.updated_prompt_stages.append(self.prompt_stages[0]+self.topic)
+            else:
+                self.updated_prompt_stages.append(self.prompt_stages[i])
+    def custom_chat(self, question):
+        self.messages.append({"role": "user", "content": question})
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            messages=self.messages,
+            max_tokens=self.token,
+        )
+        self.messages.append({"role": "assistant", "content": response.choices[0].message.content})
+        return response.choices[0].message.content
     def chat(self, stage):
-        self.messages.append({"role": "user", "content": str(self.prompt_stages[stage])})
+        self.messages.append({"role": "user", "content": str(self.updated_prompt_stages[stage])})
         response = openai.ChatCompletion.create(
             model=self.model,
             messages=self.messages,
@@ -31,17 +45,17 @@ class TutorAI:
     def run(self):
         self.topic = input("Topic: ")
         self.add_topic(self.topic)
-        i=1  # start at stage 1
+        i=0  # start at stage 1
         print(self.chat(i))  # convert stage number to string before passing to chat method
         while True:
-            user_input = input("Type 'next' to go to the next stage or 'exit' to exit:")
+            user_input = input("Type 'next' to go to the next stage or 'exit' to exit: ")
             if user_input == "exit":
                 break
             if user_input == "next":
                 i += 1
                 print(self.chat(i))
             else:
-                print(self.chat(i))  # also convert stage number to string here
+                print(self.custom_chat(user_input))  # also convert stage number to string here
 
 
 if __name__ == "__main__":
