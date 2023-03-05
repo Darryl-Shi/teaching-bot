f26825abd9b48123e1fec699c4f2471c817a0646
Darryl-Shi <221474B@student.hci.edu.sg>
2023-03-05 19:20:55 +0800

    v2 still not working

--- main.py
+++ main.py
@@ -5,53 +5,45 @@ import json
 
 
 class TutorAI:
-    def __init__(self, question):
+    def __init__(self):
         load_dotenv()
         self.api_key = os.getenv("API_KEY")
-        self.model = "gpt-3.5-turbo"
+        self.model = os.getenv("MODEL")
         self.subtopics = []
-        self.prompt=os.getenv("PROMPT")
         self.messages = []
         self.token = int(os.getenv("TOKEN"))
+        self.prompt_stages = ["A student wants to learn about a topic, generate 4 modules that a student can use to learn and number it. A module consists of a title and a description, separated by a colon. The topic is ", "Create an outline with 4 sections for teaching a student about the topic and module and number it. If you receive the input 'back', go back to the previous stage.","Teach a student about the below topic and subtopic and by writing multiple paragraphs and number it. If you receive the input 'back', go back to the previous stage"]
+        self.topic = ""
         openai.api_key = self.api_key
 
-    def chat(self, message, firstrun, token):
-        if firstrun == True:
-            message = self.prompt + message
-            self.messages.append({"role": "system", "content": message})
-            response = openai.ChatCompletion.create(model=self.model, messages=self.messages, max_tokens=token)
-            self.messages.append({"role": "assistant", "content": self.json_parse(str(response.choices[0].message))})
-            return self.json_parse(str(response.choices[0].message))
-        else:
-            self.messages.append({"role": "system", "content": message})
-            response = openai.ChatCompletion.create(model=self.model, messages=self.messages, max_tokens=token)
-            self.messages.append({"role": "assistant", "content": self.json_parse(str(response.choices[0].message))})
-            return self.json_parse(str(response.choices[0].message))
-    def json_parse(self, data):
-        data = json.loads(data)
-        return data["content"]
-    def add_to_array(self, data):
-        data = data.split("\n")
-        for i in data:
-            if len(i) > 0 and i[0].isdigit():
-                self.subtopics.append(i[3:])
-
-    def explanation(self, subtopic_no):
-        exp = openai.ChatCompletion.create(model=self.model, messages=[{"role": "user", "content": "Explain " + subtopic_no}], max_tokens=1000)
-        return json.loads(str(exp.choices[0].message))["content"]
-
+    def add_topic(self, topic):
+        self.prompt_stages[0] = self.prompt_stages[0]+topic
+    def chat(self, stage):
+        self.messages.append({"role": "user", "content": str(self.prompt_stages[stage])})
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            messages=self.messages,
+            max_tokens=self.token,
+        )
+        self.messages.append({"role": "assistant", "content": response.choices[0].message.content})
+        return response.choices[0].message.content
+    
     def run(self):
-        print(self.chat(question, firstrun=True, token=100))
-        string = "\n".join(self.subtopics)
-        print(string)
+        self.topic = input("Topic: ")
+        self.add_topic(self.topic)
+        i=1  # start at stage 1
+        print(self.chat(i))  # convert stage number to string before passing to chat method
         while True:
-            subtopic_no = input("Enter a number or ask a question(or exit to quit): ")
-            if subtopic_no == "exit":
+            user_input = input("Type 'next' to go to the next stage or 'exit' to exit:")
+            if user_input == "exit":
                 break
-            print(self.chat(message=subtopic_no, firstrun=False, token=self.token))
+            if user_input == "next":
+                i += 1
+                print(self.chat(i))
+            else:
+                print(self.chat(i))  # also convert stage number to string here
+
 
 if __name__ == "__main__":
-    question = input("Enter a topic: ")
-
-    tutor = TutorAI(question)
+    tutor = TutorAI()
     tutor.run()
